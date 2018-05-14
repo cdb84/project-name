83536fd4a094b17a902d1e7423d0f9f71e899167
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-05-14 13:38:50 -0400

    Update heathen.py

--- heathen.py
+++ heathen.py
@@ -1,7 +1,21 @@
 #/usr/bin/python
 """
-Heathen Webserver: a webserver that does back-end preprocessing in whatever
-language you want, be it C, Fortran, or Assembly.
+    Heathen Webserver: a webserver that does back-end preprocessing in whatever
+    language you want, be it C, Fortran, or Assembly.
+
+    Copyright 2018 Connor Berry
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and 
+    limitations under the License.
 """
 import subprocess
 import sys
